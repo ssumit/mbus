c1eb9f0240ce21e0b474526f58ea448b31c0f465
ssumit <singhsumitbit@gmail.com>
2015-09-05 13:05:39 +0530

    updates queue service contract docs

--- src/main/java/com/example/QueueService.java
+++ src/main/java/com/example/QueueService.java
@@ -6,32 +6,40 @@ public interface QueueService {
 
     /**
      * Creates a new queue.
+     *
      * @param queueConfig - contains all the params that needs to be over-written.
      * @return Unique queue id.
      */
     public CompletableFuture<String> createQueue(QueueConfig queueConfig);
 
+    /**
+     * sends a single message to a particular queue.
+     * @param queueId unique queue id.
+     * @param queueMessage queue message to be sent
+     * @return
+     */
     public CompletableFuture<Void> sendMessage(String queueId, QueueMessage queueMessage);
 
+    /**
+     * retrives a single message from a particular queue.
+     * @param queueId unique queue id.
+     * @return retrieved queue message.
+     */
     public CompletableFuture<QueueMessage> retrieveMessage(String queueId);
 
-    public CompletableFuture<Void> deleteMessage(String queueId, String messageId);
+    /**
+     * deletes a single message from a particular queue.
+     * @param queueId unique queue id.
+     * @param queueMessage queue message to be sent
+     * @return
+     */
+    public CompletableFuture<Void> deleteMessage(String queueId, QueueMessage queueMessage);
 
+    /**
+     * Removes a new queue.
+     *
+     * @param queueId unique queue id.
+     * @return.
+     */
     public CompletableFuture<String> removeQueue(String queueId);
-  //
-  // Task 1: Define me.
-  //
-  // This interface should include the following methods.  You should choose appropriate
-  // signatures for these methods that prioritise simplicity of implementation for the range of
-  // intended implementations (in-memory, file, and SQS).  You may include additional methods if
-  // you choose.
-  //
-  // - push
-  //   pushes a message onto a queue.
-  // - pull
-  //   retrieves a single message from a queue.
-  // - delete
-  //   deletes a message from the queue that was received by pull().
-  //
-
 }
